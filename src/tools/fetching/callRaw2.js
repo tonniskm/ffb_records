1239c3fa905bdd4ceda5037cd136e37337843940
tonni <kevin.tonnis@gmail.com>
2025-12-23 11:26:56 -0500

    sleeper playoffs

--- src/tools/fetching/callRaw2.js
+++ src/tools/fetching/callRaw2.js
@@ -37,13 +37,13 @@ export async function callRaw(vars,setRaw){
             for (let year=lastSavedYear;year<=yearMax;year++){
                 if(year<2025){continue}
                 out[year] = []
+                let wBracket = []
+                let lBracket = []
                 for(let week=1;week<nflState.week;week++){
                 const matchupsRes = await fetch(`https://api.sleeper.app/v1/league/${sleeper_league_id}/matchups/${week}`);
                 const matchups = await matchupsRes.json();
                 const used_mids = []
                 const mid_inds = {}
-                let wBracket = []
-                let lBracket = []
                 for(const matchup of matchups){
                     const mid = matchup.matchup_id
                     const rid = matchup.roster_id
@@ -54,8 +54,42 @@ export async function callRaw(vars,setRaw){
                             if(wBracket.length<2){type='P1';wBracket.push(sleeperNames[uid]?.ind)}
                             else{type='L1';lBracket.push(sleeperNames[uid]?.ind)}
                         }
-                        else if(mid==1 || mid==2){type='P1';wBracket.push(sleeperNames[uid]?.ind)}
-                        else if(mid==4 || mid==5){type='L1';lBracket.push(sleeperNames[uid]?.ind)}
+                        else if(mid==1 || mid==2){type='P1';
+                            let s1,s2
+                            if(used_mids.includes(mid)){
+                            s1 = out[year][mid_inds[mid]]['Score1']
+                            s2 = matchup.points
+                            wBracket.push(s1>s2?out[year][mid_inds[mid]]['Team1']:sleeperNames[uid]?.ind)}
+                        }
+                        else if(mid==4 || mid==5){type='L1';
+                            let s1,s2
+                            if(used_mids.includes(mid)){
+                            s1 = out[year][mid_inds[mid]]['Score1']
+                            s2 = matchup.points
+                            lBracket.push(s2>s1?out[year][mid_inds[mid]]['Team1']:sleeperNames[uid]?.ind)}
+                        }
+                    }
+                    if(week==16){
+                        let s1,s2
+                        if(used_mids.includes(mid)){
+                            s1 = out[year][mid_inds[mid]]['Score1']
+                            s2 = matchup.points
+                            if(mid==1||mid==2){type='P2';
+                                s2>s1?wBracket=wBracket.filter(x=>x!==out[year][mid_inds[mid]]['Team1']):wBracket=wBracket.filter(x=>x!==sleeperNames[uid]?.ind)}
+                            if(mid==3||mid==6){type='lame'}
+                            if(mid==4||mid==5){type='L2';
+                                s1>s2?lBracket=lBracket.filter(x=>x!==out[year][mid_inds[mid]]['Team1']):lBracket=lBracket.filter(x=>x!==sleeperNames[uid]?.ind)}
+                        }
+                    }
+                    if(week==17){
+                        let s1,s2
+                        if(used_mids.includes(mid)){
+                            s1 = out[year][mid_inds[mid]]['Score1']
+                            s2 = matchup.points
+                            if(mid==1){type='P3';s2>s1?wBracket=wBracket.filter(x=>x!==out[year][mid_inds[mid]]['Team1']):wBracket=wBracket.filter(x=>x!==sleeperNames[uid]?.ind)}
+                            if([2,3,5,6].includes(mid)){type='lame'}
+                            if(mid==4){type='L3';s1>s2?lBracket=lBracket.filter(x=>x!==out[year][mid_inds[mid]]['Team1']):lBracket=lBracket.filter(x=>x!==sleeperNames[uid]?.ind)}
+                        }
                     }
 
                     if(used_mids.includes(mid)){
